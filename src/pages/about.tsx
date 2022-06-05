faa656f5de0ec4b3ea5f637df44d5e3ba72f12db
Małgorzata Derkacz <malgorzataderkacz3@gmail.com>
2022-06-06 01:56:09 +0200

    fix(About): add rel=noreferrer in hyperlinks

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -6,9 +6,9 @@ export const about = () => {
             <img className='gif' src="author.jpg" alt="miku is glad you're here"></img>
             <p>Gosia</p>
             <br></br>
-            <p>github: <a href="https://github.com/g0sie" target="_blank">g0sie</a></p>
-            <p>instagram: <a href="https://www.instagram.com/g0sie/" target="_blank">g0sie</a></p>
-            <p>repozytorium: <a href='https://github.com/g0sie/react-test' target="_blank">repozytorium</a></p>
+            <p>github: <a href="https://github.com/g0sie" target="_blank" rel="noreferrer">g0sie</a></p>
+            <p>instagram: <a href="https://www.instagram.com/g0sie/" target="_blank" rel="noreferrer">g0sie</a></p>
+            <p>repozytorium: <a href='https://github.com/g0sie/react-test' target="_blank" rel="noreferrer">repozytorium</a></p>
             <br></br>
         </div>
     );
